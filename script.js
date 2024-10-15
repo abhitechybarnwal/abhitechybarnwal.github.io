3e3b992c6e553087985c31c5a6dcebdeb7e9a864
anishghosh333 <anighoh@jacobs-university.de>
2024-10-15 04:30:03 -0400

    Improved neural network visualization with fun features

--- script.js
+++ script.js
@@ -15,31 +15,31 @@ document.addEventListener('DOMContentLoaded', function() {
     function displayNeuronInfo(neuronId) {
         switch(neuronId) {
             case 'input1':
-                infoBox.innerHTML = 'Input Neuron 1 (x1): Receives input data from the dataset, such as features like pixel intensities or numeric values.';
+                infoBox.innerHTML = 'Input Brain x1: This brain receives information like "How sunny is it?" and sends it to the hidden brains!';
                 break;
             case 'input2':
-                infoBox.innerHTML = 'Input Neuron 2 (x2): Another input data point. Neural networks can have multiple inputs corresponding to different features.';
+                infoBox.innerHTML = 'Input Brain x2: This brain might know "What temperature is it?" and works with other brains to make decisions.';
                 break;
             case 'input3':
-                infoBox.innerHTML = 'Input Neuron 3 (x3): Just like x1 and x2, it is part of the input layer that sends data to the hidden layers.';
+                infoBox.innerHTML = 'Input Brain x3: Like its friends, this brain takes in cool info and helps the whole team learn!';
                 break;
             case 'hidden1':
-                infoBox.innerHTML = 'Hidden Neuron 1 (h1): Processes input data by applying weights and an activation function, transforming input into something more meaningful.';
+                infoBox.innerHTML = 'Hidden Brain h1: This brain takes the info from x1, x2, and x3 and makes it more useful for the final decision!';
                 break;
             case 'hidden2':
-                infoBox.innerHTML = 'Hidden Neuron 2 (h2): A key neuron in the hidden layer, often responsible for learning complex patterns in the data.';
+                infoBox.innerHTML = 'Hidden Brain h2: Another brain in the middle! It helps the network learn by figuring out patterns.';
                 break;
             case 'hidden3':
-                infoBox.innerHTML = 'Hidden Neuron 3 (h3): Further processes the input before sending it to the output layer.';
+                infoBox.innerHTML = 'Hidden Brain h3: This brain is super smart! It helps the network understand complex things like if it\'s a sunny day!';
                 break;
             case 'output1':
-                infoBox.innerHTML = 'Output Neuron 1 (y1): Outputs the final result (e.g., classification as "cat" or "dog" in an image recognition task).';
+                infoBox.innerHTML = 'Output Brain y1: "Hmm, I think today is a good day to play outside!" This brain makes the final decision.';
                 break;
             case 'output2':
-                infoBox.innerHTML = 'Output Neuron 2 (y2): Another output, typically used when dealing with multi-class classification problems.';
+                infoBox.innerHTML = 'Output Brain y2: "Maybe we should stay indoors." It helps decide based on all the info from other brains.';
                 break;
             default:
-                infoBox.innerHTML = 'Click on a neuron to learn more!';
+                infoBox.innerHTML = 'Click on a brain to find out what it does!';
         }
     }
 });
